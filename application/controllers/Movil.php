05ba470c69ba040e3ac82d1c2d98711210d3b7cb
multierpio <47793681+multierpio@users.noreply.github.com>
2019-07-11 22:12:44 +0200

    Update Movil

--- application/controllers/Movil.php
+++ application/controllers/Movil.php
@@ -1,56 +1,162 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+
 
 class Movil extends Base_Controller {
 
+
+
 	public function __construct(){
+
 		parent::__construct();
 
+
+
 		//Carga de Modelo
+
 		$this->load->model('modulo_usuario/usuario_model');
 
+		$this->load->model('establecimientosusuarios_model');
+
+		$this->load->model('establecimientos_model');
+
+		$this->load->model('registros_model');
+
+
+
+
 
 	}
+
+
 
 	public function login()
+
 	{
-  		$login_correo = $this->input->post('f_login_correo');
-		$login_password = $this->input->post('f_login_password');
-		// $login_password = md5($login_password);
+
 		
+
+  		$login_correo = $this->input->post('f_login_correo');
+
+		$login_password = $this->input->post('f_login_password');
+
+		
+
+		//Prueba
+
+		
+
+		$login_correo = "demo@verificar.me";
+
+		$login_password = "c514c91e4ed341f263e458d44b3bb0a7";
+
+		
+
 		$validacion = $this->usuario_model->login($login_correo, $login_password);
 
+
+
 		if($validacion){
+
 			$usuario = array(
+
 				'conectado' => 1, 
+
 				'usuario_id' => $validacion->id, 
+
 				'usuario_validado' => $validacion->validado, 
+
 				'usuario_nombre' => $validacion->nombre, 
+
 				'usuario_apellido' => $validacion->apellido, 
+
 				'usuario_nombre_completo' => $validacion->apellido.' '.$validacion->nombre, 
+
 				'usuario_correo' => $validacion->correo
+
 			);
+
 		}else{
+
 			$usuario = array();
+
 		}
+
     		return $usuario;
-	}
-
-	public function obtenerLocalesUsuario(){
-		//Obtener id usuario y obtener los locales a los que 
-		//pertenece en la por la tabla "persona-locales" luego devolver un array con los locales
-		
-		$idUsuario = $this->input->get('idUsuario');
-		
 
 	}
+
+
+
+	public function obtenerLocalesUsuario($idUsuario){
+
 	
-	public function insertarHora(){
-		//Obtiene el id usuario y la hora de entrada o salida diferenciada por una letra al principio del texto (E o S)
-		// y la inserta en la bd del usuario
+
+		$arrayLocales2 = array(); 
+
 		
-		$idUsuario = $this->input->get('idUsuario');
-		$hora = $this->input->get('hora');
+
+		$arrayLocales = $this->establecimientosusuarios_model->obtener_por_id_usuario($idUsuario);
+
+		
+
+		foreach ($arrayLocales as $row){
+
+      	    $id=  $row->id_local;
+
+      	    $local = $this->establecimientos_model->obtener($id);
+
+      	    array_push($arrayLocales2, $local);
+
+			
+
+  		}
+
+
+
+		return $arrayLocales2;
+
+
+
 	}
+
+	
+
+	public function insertarHora(){
+
+		
+
+		$idUsuario = $this->input->post('idUsuario');
+
+		$idLocal = $this->input->post('idUsuario');
+
+		$tipo =  $this->input->post('tipo');
+
+		$hora = $this->input->get('hora');
+
+		
+
+		$data = array(
+
+        'id_persona' => $idUsuario,
+
+        'id_local'  => $idLocal,
+
+        'tipo'  => $tipo,
+
+        'fecha'  => $hora,
+
+        'fecha_serv'  => date("G:H:s")
+
+		);
+
+		$this->registros_model->alta($data);
+
+	}
+
   
+
 }
+
