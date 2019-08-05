691907b1d9278c05fcf9c029eb5306891e04eb28
Jonathan <llamasjonat@gmail.com>
2019-08-05 09:41:20 -0300

    Update

--- application/views/template/version_1/header.php
+++ application/views/template/version_1/header.php
@@ -59,71 +59,92 @@
     </div>
 
     <header class="page-topbar" id="header">
-      <div class="navbar navbar-fixed">
-        <nav class="navbar-main navbar-color nav-collapsible sideNav-lock navbar-dark gradient-45deg-indigo-purple no-shadow">
-          <div class="nav-wrapper">
-            <!-- <div class="header-search-wrapper hide-on-med-and-down"><i class="material-icons">search</i>
-              <input class="header-search-input z-depth-2" type="text" name="Search" placeholder="Busca aqui empleados, establecimientos ...">
-            </div> -->
-            <ul class="navbar-list right">
-              <!-- <li class="hide-on-med-and-down"><a class="waves-effect waves-block waves-light translation-button" href="javascript:void(0);" data-target="translation-dropdown"><span class="flag-icon flag-icon-gb"></span></a></li> -->
-              <li class="hide-on-med-and-down"><a class="waves-effect waves-block waves-light toggle-fullscreen" href="javascript:void(0);"><i class="material-icons">settings_overscan</i></a></li>
-              <li class="hide-on-large-only"><a class="waves-effect waves-block waves-light search-button" href="javascript:void(0);"><i class="material-icons">search</i></a></li>
-              <li><a class="waves-effect waves-block waves-light notification-button" href="javascript:void(0);" data-target="notifications-dropdown"><i class="material-icons">notifications_none<small class="notification-badge pulse">5</small></i></a></li>
-              <li><a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);" data-target="profile-dropdown"><span class="avatar-status avatar-online"><img src="<?php echo base_url(); ?>assets/css/images/profile_user_default.png" alt="avatar"><i></i></span></a></li>
-              <li><a class="waves-effect waves-block waves-light sidenav-trigger" href="#" data-target="slide-out-right"><i class="material-icons">format_indent_increase</i></a></li>
-            </ul>
-            <!-- <ul class="dropdown-content" id="translation-dropdown">
-              <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-gb"></i> English</a></li>
-              <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-fr"></i> French</a></li>
-              <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-cn"></i> Chinese</a></li>
-              <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-de"></i> German</a></li>
-            </ul> -->
-            <ul class="dropdown-content" id="notifications-dropdown">
-              <li>
-                <h6>NOTIFICATIONS<span class="new badge">5</span></h6>
-              </li>
-              <li class="divider"></li>
-              <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle cyan small">add_shopping_cart</span> $prueba->nombre</a>
-                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">2 hours ago</time>
-              </li>
-              <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle red small">stars</span> Completed the task</a>
-                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">3 days ago</time>
-              </li>
-              <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle teal small">settings</span> Settings updated</a>
-                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">4 days ago</time>
-              </li>
-              <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle deep-orange small">today</span> Director meeting started</a>
-                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">6 days ago</time>
-              </li>
-              <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle amber small">trending_up</span> Generate monthly report</a>
-                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">1 week ago</time>
-              </li>
-            </ul>
-            <ul class="dropdown-content" id="profile-dropdown">
-              <!-- <li><a class="grey-text text-darken-1" href="user-profile-page.html"><i class="material-icons">person_outline</i> Profile</a></li> -->
-              <!-- <li><a class="grey-text text-darken-1" href="app-chat.html"><i class="material-icons">chat_bubble_outline</i> Chat</a></li> -->
-              <!-- <li><a class="grey-text text-darken-1" href="page-faq.html"><i class="material-icons">help_outline</i> Help</a></li> -->
-              <!-- <li class="divider"></li> -->
-              <!-- <li><a class="grey-text text-darken-1" href="user-lock-screen.html"><i class="material-icons">lock_outline</i> Lock</a></li> -->
-              <li><a class="grey-text text-darken-1" href="<?php echo base_url(); ?>panel/salir"><i class="material-icons">keyboard_tab</i> Logout</a></li>
-            </ul>
-          </div>
-          <nav class="display-none search-sm">
-            <div class="nav-wrapper">
-              <form>
-                <div class="input-field">
-                  <input class="search-box-sm" type="search" required="">
-                  <label class="label-icon" for="search"><i class="material-icons search-sm-icon">search</i></label><i class="material-icons search-sm-close">close</i>
+        <div class="navbar navbar-fixed">
+            <nav class="navbar-main navbar-color nav-collapsible sideNav-lock navbar-dark gradient-45deg-indigo-purple no-shadow">
+                <div class="nav-wrapper">
+                    <!-- <div class="header-search-wrapper hide-on-med-and-down"><i class="material-icons">search</i>
+                      <input class="header-search-input z-depth-2" type="text" name="Search" placeholder="Busca aqui empleados, establecimientos ...">
+                    </d -->iv>
+                    <?php 
+
+                        $notificaciones = 0;
+                        if($cuenta->cant_empresas == $cuenta->suscripcion_cant_empresas){
+                            $notificaciones++;
+                        }
+                        if($cuenta->cant_establecimientos == $cuenta->suscripcion_cant_establecimientos){
+                            $notificaciones++;
+                        }
+
+                    ?>
+                    <ul class="navbar-list right">
+                        <!-- <li class="hide-on-med-and-down"><a class="waves-effect waves-block waves-light translation-button" href="javascript:void(0);" data-target="translation-dropdown"><span class="flag-icon flag-icon-gb"></span></a></li> -->
+                        <li class="hide-on-med-and-down"><a class="waves-effect waves-block waves-light toggle-fullscreen" href="javascript:void(0);"><i class="material-icons">settings_overscan</i></a></li>
+                        <li class="hide-on-large-only"><a class="waves-effect waves-block waves-light search-button" href="javascript:void(0);"><i class="material-icons">search</i></a></li>
+                        <li>
+                            <a class="waves-effect waves-block waves-light notification-button" href="javascript:void(0);" data-target="notifications-dropdown">
+                                <i class="material-icons">
+                                    notifications_none
+                                    <?php if($notificaciones != 0){ ?>
+                                    <small class="notification-badge pulse"><?php echo $notificaciones; ?></small>
+                                    <?php } ?>
+                                </i>
+                            </a>
+                        </li>
+                        <li><a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);" data-target="profile-dropdown"><span class="avatar-status avatar-online"><img src="<?php echo base_url(); ?>assets/css/images/profile_user_default.png" alt="avatar"><i></i></span></a></li>
+                        <li><a class="waves-effect waves-block waves-light sidenav-trigger" href="#" data-target="slide-out-right"><i class="material-icons">format_indent_increase</i></a></li>
+                    </ul>
+                    <!-- <ul class="dropdown-content" id="translation-dropdown">
+                        <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-gb"></i> English</a></li>
+                        <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-fr"></i> French</a></li>
+                        <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-cn"></i> Chinese</a></li>
+                        <li><a class="grey-text text-darken-1" href="#!"><i class="flag-icon flag-icon-de"></i> German</a></li>
+                    </ul> -->
+                    <ul class="dropdown-content" id="notifications-dropdown">
+                        <li>
+                            <h6>
+                                NOTIFICATIONS
+                                <?php if($notificaciones != 0){ ?>
+                                <span class="new badge"><?php echo $notificaciones; ?></span>
+                                <?php } ?>
+                            </h6>
+                        </li>
+                        <li class="divider"></li>
+                        <?php if($cuenta->cant_empresas == $cuenta->suscripcion_cant_empresas){ ?>
+                        <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle red small">settings</span> Límite de Empresas</a>
+                            <p class="media-meta">Ha alcanzado el límite máximo de empresas por crear.</p>
+                        </li>
+                        <?php } ?>
+                        <?php if($cuenta->cant_establecimientos == $cuenta->suscripcion_cant_establecimientos){ ?>
+                        <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle red small">settings</span> Límite de Establecimientos</a>
+                            <p class="media-meta">Ha alcanzado el límite máximo de establecimientos por crear.</p>
+                        </li>
+                        <?php } ?>
+                    </ul>
+                    <ul class="dropdown-content" id="profile-dropdown">
+                        <!-- <li><a class="grey-text text-darken-1" href="user-profile-page.html"><i class="material-icons">person_outline</i> Profile</a></li> -->
+                        <!-- <li><a class="grey-text text-darken-1" href="app-chat.html"><i class="material-icons">chat_bubble_outline</i> Chat</a></li> -->
+                        <!-- <li><a class="grey-text text-darken-1" href="page-faq.html"><i class="material-icons">help_outline</i> Help</a></li> -->
+                        <!-- <li class="divider"></li> -->
+                        <!-- <li><a class="grey-text text-darken-1" href="user-lock-screen.html"><i class="material-icons">lock_outline</i> Lock</a></li> -->
+
+                        <li><a class="grey-text text-darken-1" href="<?php echo base_url(); ?>panel/salir"><i class="material-icons">keyboard_tab</i> Logout</a></li>
+                    </ul>
                 </div>
-              </form>
-            </div>
-          </nav>
-        </nav>
-      </div>
+                <nav class="display-none search-sm">
+                    <div class="nav-wrapper">
+                        <form>
+                            <div class="input-field">
+                                <input class="search-box-sm" type="search" required="">
+                                <label class="label-icon" for="search"><i class="material-icons search-sm-icon">search</i></label><i class="material-icons search-sm-close">close</i>
+                            </div>
+                        </form>
+                    </div>
+                </nav>
+            </nav>
+        </div>
     </header>
 
     <?php echo $menu; ?>
     
     <div id="main">
-      <div class="row">
+        <div class="row">
